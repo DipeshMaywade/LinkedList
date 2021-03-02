3bba68d6356a5d0c63fa64c4c25103f86eeeff7b
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-03-02 21:18:51 +0530

    Updated: Creating Queue using LinkedList append method for abilityto push values in Queue

---
+++ main/java/MyQueue.java
@@ -0,0 +1,20 @@
+public class MyQueue {
+
+    public MyLinkedList myLinkedList;
+
+    public MyQueue(){
+        this.myLinkedList = new MyLinkedList();
+    }
+
+    public INode push(INode myNode) {
+        return myLinkedList.append(myNode);
+    }
+
+    public void printQueue() {
+        myLinkedList.printMyNode();
+    }
+
+    public INode peak() {
+        return myLinkedList.head;
+    }
+}
---
+++ test/java/MyQueueTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyQueueTest {
+    @Test
+    public void given3numbersInQueueFirstPushedNodeShouldBeTOS(){
+        MyNode<Integer> firstNode = new MyNode<>(70);
+        MyNode<Integer> secondNode = new MyNode<>(30);
+        MyNode<Integer> thirdNode = new MyNode<>(56);
+
+        MyQueue myQueue = new MyQueue();
+
+        myQueue.push(thirdNode);
+        myQueue.push(secondNode);
+        myQueue.push(firstNode);
+        myQueue.printQueue();
+        INode peak = myQueue.peak();
+
+        Assert.assertEquals(thirdNode,peak);
+    }
+}
