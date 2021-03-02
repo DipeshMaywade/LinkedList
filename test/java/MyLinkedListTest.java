678578869c07175fed591fcadb5fd3f1afd867b9
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-03-02 14:30:14 +0530

    Updated: updated code to creating linked list by insert method 30 BW 56 to 70

--- test/java/MyLinkedListTest.java
+++ test/java/MyLinkedListTest.java
@@ -36,4 +36,21 @@ public class MyLinkedListTest {
                 myLinkedList.tail.equals(thirdNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3numbersWhenLinkedListShouldBeInsertToBetween() {
+        MyNode<Integer> firstNode = new MyNode<>(56);
+        MyNode<Integer> secondNode = new MyNode<>(30);
+        MyNode<Integer> thirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.append(firstNode);
+        myLinkedList.append(thirdNode);
+        myLinkedList.insert(firstNode,secondNode);
+        myLinkedList.printMyNode();
+
+        boolean result = myLinkedList.head.equals(firstNode) &&
+                myLinkedList.head.getNext().equals(secondNode) &&
+                myLinkedList.tail.equals(thirdNode);
+        Assert.assertTrue(result);
+    }
 }
