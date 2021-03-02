9593cec90cd1e6e58a41ff87364901ca15b0aee2
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-03-02 14:13:42 +0530

    Updated: updated code to creating linked list by appending 30 and 70 to 50

--- test/java/MyLinkedListTest.java
+++ test/java/MyLinkedListTest.java
@@ -19,4 +19,21 @@ public class MyLinkedListTest {
                          myLinkedList.tail.equals(firstNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3numbersWhenLinkedListShouldBeAddedToLast() {
+        MyNode<Integer> firstNode = new MyNode<>(56);
+        MyNode<Integer> secondNode = new MyNode<>(30);
+        MyNode<Integer> thirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.append(firstNode);
+        myLinkedList.append(secondNode);
+        myLinkedList.append(thirdNode);
+        myLinkedList.printMyNode();
+
+        boolean result = myLinkedList.head.equals(firstNode) &&
+                myLinkedList.head.getNext().equals(secondNode) &&
+                myLinkedList.tail.equals(thirdNode);
+        Assert.assertTrue(result);
+    }
 }
